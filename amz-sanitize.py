797394fc58026c19d02a60bfe438b91d9dff632d
Max <maxim.kasyanenko@protonmail.ch>
2020-10-10 14:28:14 -0400

    Save changes

--- amz-sanitize.py
+++ amz-sanitize.py
@@ -5,10 +5,32 @@ some fixed number of lines per CSV file.
 """
 import os
 import sys
-import json
-import pandas as pd
+import threading as th
+import multiprocessing as mp
 from pathlib import Path
 
+
+# Determines whether multithreading should be employed
+# to speed up JSON document processing.
+#
+# This option may hurt performance on single threaded systems.
+threading_enable = True
+
+# For multithreaded parsing of the JSON documents,
+# we have to optimally distribute the workload across
+# available logical processors.
+#
+# Obviously we rely on the OS to schedule operations for
+# us, but they tend to be good at that.
+threading_max_threads = mp.cpu_count()
+
+# For the multithreaded implementation of the JSON document parser,
+# we define the maximum number of line entries that will be processed
+# in blocks by multiple threads.
+#
+# On an 8-logical processor system this will give each thread about
+# 8,000 lines to chew through.
+threading_max_line_buffer_size = 2 ** 16
 
 # Value in the range [0, 1] specifying the portion,
 # as a percentage, of each dataset to convert to
@@ -19,42 +41,107 @@ max_dataset_portion = 0.25
 # 64 files for the 9 GB "json".
 max_csv_lines = 131072
 
+""" multithreading pseudocode
+ReadTS
+----------------------------
+ready: Boolean
+read_cv: ConditionVariable
+proc_cv: ConditionVariable
+meet: Barrier
 
-def raw_to_json(amz_ds):
+
+Read Thread (manager thread)         | Parsing Thread (subordinate thread) |
+-------------------------------------|-------------------------------------|
+launch subordinate threads           |                                     |
+while needs_content:                 |while !readts.ready                  |
+  fill line buffer                   |  readts.read_cv.wait()              |
+  readts.read_cv.notify_all()        |                                     |
+  while !all_threads_copied_data:    |                                     |
+    readts.proc_cv.wait()            |                                     |
+                                     |                                     |
+                                     |                                     |
+                                     |                                     |
+                                     |                                     |
+                                     |                                     |
+                                     |                                     |
+                                     |                                     |
+                                     |                                     |
+                                     |                                     |
+"""
+
+class ParsingThread(th.Thread):
+
+	def __init__(self, readts, lnbuf, index, blksz):
+		"""
+		:param readts
+			An object containing the reading thread state. The reading
+			thread will manage all subordinate ParsingThreads.
+
+				readts.ready : Boolean - Whether the global line buffer is
+										 ready to be read from and parsed.
+				
+				readts.
+
+		:param lnbuf
+			The line buffer created by the raw_to_csv_mp() function
+			where the reading thread will fill the data.
+		
+		:param index
+			The index in the lnbuf from which this thread will
+			being parsing.
+		
+		:param blksz
+			The number of elements in total this thread shall process
+			in the line buffer.
+		
+		The inteval of lines this thread will process is [index, index + blksz)
+		"""
+		self._local = th.local()
+		self._local.index = index
+		self._local.blksz = blksz
+		self._local.raw_block = [] * blksz
+		self._local.parsed_block = [] * blksz
+		self.lnbuf = lnbuf
+
+	def run(self):
+		pass
+
+
+def raw_to_csv_mp(amz_ds): 
+	"""
+	The multithreaded implementation of raw_to_csv()
+	"""
+	# ensure that the threading settings are
+	# valid enough to perform efficiently.
+	lnbfsz = threading_max_line_buffer_size
+	threads = threading_max_threads
+
+	is_pow2 = 0 == lnbfsz & (lnbfsz - 1)
+	is_gtcpus = lnbfsz > threads
+	if not (is_pow2 and is_gtcpus):
+		raise RuntimeError("Line buffer size must be a power of 2; must be greater than processor count {threads}")
+
+	# setup the initial data
 	raw = amz_ds.joinpath("raw")
 	json_f = raw.joinpath("json")
-	json_c = 0
+	csv_f = raw.joinpath("csv")
+	line_buffer = [] * threading_max_line_buffer_size
 
-	with json_f.open(mode="r", encoding="utf-8") as handle:
-		for i, l in enumerate(handle):
-			json_c += 1
-	json_c = int(json_c * max_dataset_portion)
+	
 
-	with json_f.open(mode="r", encoding="utf-8") as handle:
-		n_csv = 0
-		n_line = 0
-		df = pd.DataFrame([], [])
+	# setup the threads, locks, and other concurrent
+	# architecture
+	thread_block_size = lnbfsz / threads
+	thread_group = []
+	for i in range(threading_max_threads):
+		thread_group = ParsingThread(line_buffer, i * thread_block_size, thread_block_size)
 
-		for line in handle:
-			df.append(json.loads(line), ignore_index=True)
-			print(df)
-			break
-			n_line += 1
-			print("[%5.1f%%] Processed of %5.1f%% portion" % (
-				100 * n_line / json_c, 
-				100 * max_dataset_portion
-			))
-			if n_line % max_csv_lines == 0:
-				print(f"Writing CSV fragment {n_csv}")
-				df.to_csv(raw.joinpath(f"csv-{n_csv}"))
-				df.drop(df.index, inplace=True)
-				n_csv += 1
 
-			if n_line > json_c:
-				break
-		
-		if len(df.index) > 0:
-			df.to_csv(raw.joinpath(f"csv-{n_csv}"))
+
+def raw_to_csv(amz_ds):
+	if threading_enable:
+		raw_to_csv_mp(amz_ds)
+		return
 
 
 if __name__ == "__main__":
@@ -67,4 +154,4 @@ if __name__ == "__main__":
 		is_amazon = dataset.name.startswith("amz")
 		if is_amazon:
 			print(f"Converting dataset {str(dataset)}")
-			raw_to_json(dataset)
+			raw_to_csv(dataset)
